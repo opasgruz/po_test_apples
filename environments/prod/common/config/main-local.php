d7719ee41d496a749882dff7cd96779b4ee6b4b1
Ivan Dubyshkin <di@iway.ru>
2025-12-04 00:38:22 +0700

    Yii2 advanced

---
+++ environments/prod/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=mysql;dbname=yii2advanced',
+            'username' => 'yii2advanced',
+            'password' => 'secret',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
